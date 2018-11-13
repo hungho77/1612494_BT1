b617d905ff9d86112bfbfa96e0fb77ffbc3d3abd
hung <hothinhhung77@gmail.com>
2018-11-13 18:35:21 +0700

    Add project files.

---
+++ 1612494_BT1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace _1612494_BT1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_Line = new System.Windows.Forms.Button();
+            this.bt_Circle = new System.Windows.Forms.Button();
+            this.bt_BangMau = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.openGLControl = new SharpGL.OpenGLControl();
+            this.bt_Rectangle = new System.Windows.Forms.Button();
+            this.bt_Eclipse = new System.Windows.Forms.Button();
+            this.bt_EquilateralTriangle = new System.Windows.Forms.Button();
+            this.bt_equilateralPentagonal = new System.Windows.Forms.Button();
+            this.bt_equilateralHexagonal = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bt_Line
+            // 
+            this.bt_Line.Location = new System.Drawing.Point(36, 28);
+            this.bt_Line.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_Line.Name = "bt_Line";
+            this.bt_Line.Size = new System.Drawing.Size(48, 40);
+            this.bt_Line.TabIndex = 0;
+            this.bt_Line.Text = "Line";
+            this.bt_Line.UseVisualStyleBackColor = true;
+            this.bt_Line.Click += new System.EventHandler(this.bt_Line_Click);
+            // 
+            // bt_Circle
+            // 
+            this.bt_Circle.Location = new System.Drawing.Point(99, 28);
+            this.bt_Circle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_Circle.Name = "bt_Circle";
+            this.bt_Circle.Size = new System.Drawing.Size(49, 40);
+            this.bt_Circle.TabIndex = 1;
+            this.bt_Circle.Text = "Circle";
+            this.bt_Circle.UseVisualStyleBackColor = true;
+            this.bt_Circle.Click += new System.EventHandler(this.bt_Circle_Click);
+            // 
+            // bt_BangMau
+            // 
+            this.bt_BangMau.Location = new System.Drawing.Point(609, 28);
+            this.bt_BangMau.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_BangMau.Name = "bt_BangMau";
+            this.bt_BangMau.Size = new System.Drawing.Size(92, 40);
+            this.bt_BangMau.TabIndex = 2;
+            this.bt_BangMau.Text = "Bang Mau";
+            this.bt_BangMau.UseVisualStyleBackColor = true;
+            this.bt_BangMau.Click += new System.EventHandler(this.bt_BangMau_Click);
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.DrawFPS = false;
+            this.openGLControl.Location = new System.Drawing.Point(29, 73);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(662, 500);
+            this.openGLControl.TabIndex = 4;
+            this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            this.openGLControl.Resized += new System.EventHandler(this.openGLControl_Resized);
+            this.openGLControl.Load += new System.EventHandler(this.openGLControl_Load_1);
+            this.openGLControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ctrl_OpenGLControl_MouseDown);
+            this.openGLControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.ctrl_OpenGLControl_MouseMove);
+            this.openGLControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.ctrl_OpenGLControl_MouseUp);
+            // 
+            // bt_Rectangle
+            // 
+            this.bt_Rectangle.Location = new System.Drawing.Point(164, 28);
+            this.bt_Rectangle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_Rectangle.Name = "bt_Rectangle";
+            this.bt_Rectangle.Size = new System.Drawing.Size(64, 40);
+            this.bt_Rectangle.TabIndex = 5;
+            this.bt_Rectangle.Text = "Rectangle";
+            this.bt_Rectangle.UseVisualStyleBackColor = true;
+            this.bt_Rectangle.Click += new System.EventHandler(this.bt_Rectangle_Click);
+            // 
+            // bt_Eclipse
+            // 
+            this.bt_Eclipse.Location = new System.Drawing.Point(242, 28);
+            this.bt_Eclipse.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_Eclipse.Name = "bt_Eclipse";
+            this.bt_Eclipse.Size = new System.Drawing.Size(52, 40);
+            this.bt_Eclipse.TabIndex = 6;
+            this.bt_Eclipse.Text = "Eclipse";
+            this.bt_Eclipse.UseVisualStyleBackColor = true;
+            this.bt_Eclipse.Click += new System.EventHandler(this.bt_Eclipse_click);
+            // 
+            // bt_EquilateralTriangle
+            // 
+            this.bt_EquilateralTriangle.Location = new System.Drawing.Point(311, 28);
+            this.bt_EquilateralTriangle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_EquilateralTriangle.Name = "bt_EquilateralTriangle";
+            this.bt_EquilateralTriangle.Size = new System.Drawing.Size(69, 40);
+            this.bt_EquilateralTriangle.TabIndex = 7;
+            this.bt_EquilateralTriangle.TabStop = false;
+            this.bt_EquilateralTriangle.Text = "equilateral triangle";
+            this.bt_EquilateralTriangle.UseVisualStyleBackColor = true;
+            this.bt_EquilateralTriangle.Click += new System.EventHandler(this.bt_EquilateralTriangle_Click);
+            // 
+            // bt_equilateralPentagonal
+            // 
+            this.bt_equilateralPentagonal.Location = new System.Drawing.Point(400, 28);
+            this.bt_equilateralPentagonal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_equilateralPentagonal.Name = "bt_equilateralPentagonal";
+            this.bt_equilateralPentagonal.Size = new System.Drawing.Size(70, 40);
+            this.bt_equilateralPentagonal.TabIndex = 8;
+            this.bt_equilateralPentagonal.Text = "equilateral pentagonal";
+            this.bt_equilateralPentagonal.UseVisualStyleBackColor = true;
+            this.bt_equilateralPentagonal.Click += new System.EventHandler(this.bt_equilateralPentagonal_Click);
+            // 
+            // bt_equilateralHexagonal
+            // 
+            this.bt_equilateralHexagonal.Location = new System.Drawing.Point(494, 28);
+            this.bt_equilateralHexagonal.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.bt_equilateralHexagonal.Name = "bt_equilateralHexagonal";
+            this.bt_equilateralHexagonal.Size = new System.Drawing.Size(71, 40);
+            this.bt_equilateralHexagonal.TabIndex = 9;
+            this.bt_equilateralHexagonal.TabStop = false;
+            this.bt_equilateralHexagonal.Text = "equilateral hexagonal";
+            this.bt_equilateralHexagonal.UseVisualStyleBackColor = true;
+            this.bt_equilateralHexagonal.Click += new System.EventHandler(this.bt_equilateralHexagonal_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 596);
+            this.Controls.Add(this.bt_equilateralHexagonal);
+            this.Controls.Add(this.bt_equilateralPentagonal);
+            this.Controls.Add(this.bt_EquilateralTriangle);
+            this.Controls.Add(this.bt_Eclipse);
+            this.Controls.Add(this.bt_Rectangle);
+            this.Controls.Add(this.openGLControl);
+            this.Controls.Add(this.bt_BangMau);
+            this.Controls.Add(this.bt_Circle);
+            this.Controls.Add(this.bt_Line);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_Line;
+        private System.Windows.Forms.Button bt_Circle;
+        private System.Windows.Forms.Button bt_BangMau;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private SharpGL.OpenGLControl openGLControl;
+        private System.Windows.Forms.Button bt_Rectangle;
+        private System.Windows.Forms.Button bt_Eclipse;
+        private System.Windows.Forms.Button bt_EquilateralTriangle;
+        private System.Windows.Forms.Button bt_equilateralPentagonal;
+        private System.Windows.Forms.Button bt_equilateralHexagonal;
+    }
+}
+
---
+++ 1612494_BT1/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using SharpGL;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1612494_BT1
+{
+    public partial class Form1 : Form
+    {
+        Color colorUserColor;
+        short shShape; // 0 Nếu muốn vẽ đường thẳng, 1 nếu muốn vẽ đường tròn
+        private Point pStart;
+        private Point pEnd;
+        private int click = 0;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            colorUserColor = Color.White; // Gán giá trị màu ban đầu
+            shShape = 0; // Gán giá trị ban đầu
+        }
+
+        private void bt_BangMau_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                colorUserColor = colorDialog1.Color;
+            }
+        }
+
+        private void openGLControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ctrl_OpenGLControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            click = 0;
+            pEnd = e.Location;
+        }
+
+        private void ctrl_OpenGLControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            click = 1;
+            pStart = e.Location;
+            pEnd = pStart;
+
+        }
+
+        private void bt_Line_Click(object sender, EventArgs e)
+        {
+            shShape = 0;
+        }
+
+        private void bt_Circle_Click(object sender, EventArgs e)
+        {
+            shShape = 1;
+        }
+
+        private void bt_Rectangle_Click(object sender, EventArgs e)
+        {
+            shShape = 2;
+        }
+
+        private void bt_Eclipse_click(object sender, EventArgs e)
+        {
+            shShape = 3;
+        }
+
+        private void bt_EquilateralTriangle_Click(object sender, EventArgs e)
+        {
+            shShape = 4;
+        }
+
+        private void bt_equilateralPentagonal_Click(object sender, EventArgs e)
+        {
+            shShape = 5;
+        }
+
+        private void bt_equilateralHexagonal_Click(object sender, EventArgs e)
+        {
+            shShape = 6;
+        }
+
+        private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Set the clear color.
+            gl.ClearColor(0, 0, 0, 0);
+            // Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            // Load the identity.
+            gl.LoadIdentity();
+        }
+
+        private void openGLControl_Resized(object sender, EventArgs e)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+            // Load the identity.
+            gl.LoadIdentity();
+            // Create a perspective transformation.
+            gl.Viewport(0, 0, openGLControl.Width, openGLControl.Height);
+            gl.Ortho2D(0, openGLControl.Width, 0, openGLControl.Height);
+        }
+
+        private void openGLControl_OpenGLDraw(object sender, RenderEventArgs args)
+        {
+            // Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+            // Clear the color and depth buffer.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.Color(colorUserColor.R / 255.0, colorUserColor.G / 255.0, colorUserColor.B / 255.0, 0);
+
+            if (shShape == 0)
+            {
+                gl.Begin(OpenGL.GL_LINES); // chọn chế độ vẽ
+
+                gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pStart.Y);
+                gl.Vertex(pEnd.X, gl.RenderContextProvider.Height - pEnd.Y);
+
+                gl.End();
+                gl.Flush();
+            }
+            else if(shShape == 1)
+            {
+                gl.Begin(OpenGL.GL_LINE_LOOP);
+                double radius = Math.Sqrt(Math.Pow(pStart.X - pEnd.X, 2.0)
+                    + Math.Pow(pStart.Y - pEnd.Y, 2.0));
+                for (int i = 0; i <= 360; i++)
+                {
+                    double degInRad = i / 180.0 * Math.PI;
+                    gl.Vertex(Math.Cos(degInRad) * radius + pStart.X, 
+                        gl.RenderContextProvider.Height - (Math.Sin(degInRad) * radius + pStart.Y));
+                }
+                gl.End();
+                gl.Flush();
+            }
+            else if(shShape == 2)
+            {
+                gl.Begin(OpenGL.GL_LINES);
+                float delta_x = pEnd.X - pStart.X;
+                float delta_y = pEnd.Y - pStart.Y;
+                double B_x = pStart.X + delta_x;
+                double B_y = pStart.Y;
+                double C_x = pStart.X;
+                double C_y = pStart.Y + delta_y;
+                gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pStart.Y);
+                gl.Vertex(B_x, gl.RenderContextProvider.Height - B_y);
+                gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pStart.Y);
+                gl.Vertex(C_x, gl.RenderContextProvider.Height - C_y);
+                gl.Vertex(pEnd.X, gl.RenderContextProvider.Height - pEnd.Y);
+                gl.Vertex(C_x, gl.RenderContextProvider.Height - C_y);
+                gl.Vertex(B_x, gl.RenderContextProvider.Height - B_y);
+                gl.Vertex(pEnd.X, gl.RenderContextProvider.Height - pEnd.Y);
+                
+                gl.End();
+                gl.Flush();
+            }
+            else if(shShape == 3)
+            {
+                gl.Begin(OpenGL.GL_LINE_LOOP);
+
+                double a = Math.Abs(pStart.Y - pEnd.Y) / 2;
+                double b = Math.Abs(pStart.X - pEnd.X) / 2;
+                double x0 = (pStart.X + pEnd.X) / 2;
+                double y0 = (pStart.Y + pEnd.Y) / 2;
+
+                double x, y;
+                for (int i = 0; i <= 360; i++)
+                {
+                    x = b * Math.Cos(i * Math.PI / 180) + x0;
+                    y = a * Math.Sin(i * Math.PI / 180) + y0;
+                    gl.Vertex(x, gl.RenderContextProvider.Height - y);
+
+                    x = b * Math.Cos(i * Math.PI / 180) + x0;
+                    y = a * Math.Sin(i * Math.PI / 180) + y0;
+                    gl.Vertex(x, gl.RenderContextProvider.Height - y);
+                }
+                gl.End();
+                gl.Flush();
+            }
+            else if (shShape == 4)
+            {
+                gl.Begin(OpenGL.GL_LINES); // chọn chế độ vẽ
+                double h = Math.Sqrt(Math.Pow(pStart.X - pEnd.X, 2.0)
+                    + Math.Pow(pStart.Y - pEnd.Y, 2.0));
+
+                double a = 2 * h / Math.Sqrt(3.0);
+
+                double alpha = Math.Acos((pEnd.X - pStart.X) / h);
+                double A_x = Math.Cos(alpha + 30 * Math.PI / 180.0) * a + pStart.X;
+                double A_y = Math.Sin(alpha + 30 * Math.PI / 180.0) * a + pStart.Y;
+                double B_x = Math.Cos(alpha - 30 * Math.PI / 180.0) * a + pStart.X;
+                double B_y = Math.Sin(alpha - 30 * Math.PI / 180.0) * a + pStart.Y;
+
+                gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pStart.Y);
+                gl.Vertex(A_x, gl.RenderContextProvider.Height - A_y);
+                gl.Vertex(pStart.X, gl.RenderContextProvider.Height - pStart.Y);
+                gl.Vertex(B_x, gl.RenderContextProvider.Height - B_y);
+                gl.Vertex(A_x, gl.RenderContextProvider.Height - A_y);
+                gl.Vertex(B_x, gl.RenderContextProvider.Height - B_y);
+
+                gl.End();
+                gl.Flush();
+            }
+            else if(shShape == 5)
+            {
+                gl.Begin(OpenGL.GL_LINES);
+
+                double h = Math.Sqrt(Math.Pow(pStart.X - pEnd.X, 2.0)
+                    + Math.Pow(pStart.Y - pEnd.Y, 2.0));
+                double alpha = Math.Acos((pEnd.X - pStart.X) / h);
+
+                double A_x, A_y;
+                for (int i = 0; i < 5; i++)
+                {
+                    A_x = Math.Cos(alpha) * h + pStart.X;
+                    A_y = Math.Sin(alpha) * h + pStart.Y;
+                    gl.Vertex(A_x, gl.RenderContextProvider.Height - A_y);
+                    alpha += 72 * Math.PI / 180.0;
+                    A_x = Math.Cos(alpha) * h + pStart.X;
+                    A_y = Math.Sin(alpha) * h + pStart.Y;
+                    gl.Vertex(A_x, gl.RenderContextProvider.Height - A_y);
+                }
+
+                gl.End();
+                gl.Flush();
+            }
+            else if(shShape == 6)
+            {
+                gl.Begin(OpenGL.GL_LINES);
+
+                double h = Math.Sqrt(Math.Pow(pStart.X - pEnd.X, 2.0)
+                    + Math.Pow(pStart.Y - pEnd.Y, 2.0));
+                double alpha = Math.Acos((pEnd.X - pStart.X) / h);
+
+                double A_x, A_y;
+                for (int i = 0; i < 6; i++)
+                {
+                    A_x = Math.Cos(alpha) * h + pStart.X;
+                    A_y = Math.Sin(alpha) * h + pStart.Y;
+                    gl.Vertex(A_x, gl.RenderContextProvider.Height - A_y);
+                    alpha += 60 * Math.PI / 180.0;
+                    A_x = Math.Cos(alpha) * h + pStart.X;
+                    A_y = Math.Sin(alpha) * h + pStart.Y;
+                    gl.Vertex(A_x, gl.RenderContextProvider.Height - A_y);
+                }
+
+                gl.End();
+                gl.Flush();
+            }
+        }
+
+        private void ctrl_OpenGLControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(click ==1)
+                pEnd = e.Location;
+        }
+
+        private void openGLControl_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
